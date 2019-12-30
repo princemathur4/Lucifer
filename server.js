122fcb27923a9c28c20bbc1eb482619a0a98b937
princemathur4 <princemathur.mathur4@gmail.com>
2019-12-30 18:17:22 +0530

    minor fixes in server.js

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/dist'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve('public/index.html'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+ });
+ app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(port);
